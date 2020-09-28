1092a66583e8806d4dd144819968fdf8b588075d
yuanhawk <yuanmirage@yahoo.com>
2020-09-28 09:16:18 +0800

    pset 1A

--- 50.001/src/homework2_3/CheckingAccount.java
+++ 50.001/src/homework2_3/CheckingAccount.java
@@ -4,31 +4,21 @@ import session3.Account;
 
 public class CheckingAccount extends Account {
 
-    private int id = 0;
-    private double balance = 0.0;
+    private double overdraft = 5000.0;
 
     public CheckingAccount() {
     }
 
     public CheckingAccount(int id, double balance) {
         super(id, balance);
-        this.id = id;
-        this.balance = balance;
-    }
-
-    @Override
-    public void deposit(double amount) {
-        super.deposit(amount);
-        balance += amount;
     }
 
     @Override
     public void withdraw(double amount) {
         super.withdraw(amount);
-        balance -= amount;
 
-        if (balance < -5000) {
-            deposit(amount);
+        if (super.getBalance() < -5000) {
+            super.setBalance(-5000);
             System.out.println("over limit");
         }
     }
